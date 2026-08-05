6b50bbb1494fb3b008a544538681296c8a2010ae
RetroFrost <routerifter@gmail.com>
2026-08-05 06:36:39 -0300

    version: begin CreatorROM 4.0.0 development

--- unica/configs/version.sh
+++ unica/configs/version.sh
@@ -1,13 +1,12 @@
 # Copyright (c) 2025 Salvo Giangreco
+# Copyright (c) 2026 CreatorROM contributors
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# Only the below variable(s) need to be changed!
-VERSION_MAJOR=3
-VERSION_MINOR=5
-VERSION_PATCH=1
+# CreatorROM release version
+VERSION_MAJOR=4
+VERSION_MINOR=0
+VERSION_PATCH=0
 
-# The below variables will be generated automatically
-#
-# Version name
+# Generated version metadata
 ROM_VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
-ROM_CODENAME="Quant"
+ROM_CODENAME="Creator"
